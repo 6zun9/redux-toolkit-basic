4d7b8db2f6e6a524e30cbfa3d3f207f22536ba0e
Bishal Jung Rayamajhi <bishaljungrayamajhi@pop-os.localdomain>
2021-06-16 11:10:38 +0545

    main: learning basic redux-toolkit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
+import { increment, decrement, incrementByAmount } from './redux/counter';
 
 function App() {
+  const { count } = useSelector(state => state.counter)
+  const dispatch = useDispatch();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      The count is : {count}
+      <button onClick={() => dispatch(increment())}>Increment</button>
+      <button onClick={() => dispatch(decrement())}>Decrement</button>
+      <button onClick={() => dispatch(incrementByAmount(70))}>increment by 70</button>
     </div>
   );
 }
